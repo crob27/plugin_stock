bbfc8e823434896f1c9e40f8c71e97d0eb85d29b
Stephane Boireau <stephane.boireau@free.fr>
2019-08-01 14:04:25 +0200

    Page d'index

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ if ($resultat_session == 'c') {
 $user_auth = new gepiPlugIn("plugin_stock");
 $user_auth->verifDroits();
 
+include("./functions_plugin_stock.php");
 
 //**************** EN-TETE *****************
 $titre_page = "Plugin stock (1)";
@@ -32,8 +33,23 @@ require_once("../../lib/header.inc.php");
 <p class=bold><a href="../../accueil.php"><img src='../../images/icons/back.png' alt='Retour' class='back_link'/> Retour à l'accueil</a> | <a href="autre_script.php">Autre script</a></p>
 
 <h2>Plugin stock</h2>
-<p>Ce plugin ne fait absolument rien !</p>
 
+<p>Choisissez&nbsp;:</p>
+<ul>
+<?php
+
+if(calcul_autorisation_plugin_stock($_SESSION['login'], 'admin.php')) {
+	echo "
+	<li><a href='admin.php'>Administrer le plugin</a></li>
+	<li><a href='saisir_ouvrage.php'>Ajouter/supprimer des ouvrages</a></li>";
+}
+?>
+	<li><a href='reserver.php'>Réserver des ouvrages pour une période</a></li>
+	<li><a href='preter.php'>Prêter des exemplaires d'ouvrages, consulter les prêts</a></li>
+	<!--
+	<li><a href=''></a></li>
+	-->
+</ul>
 <?php
 include("../../lib/footer.inc.php");
 ?>
